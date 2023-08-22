b2078548ed0585ef2475bf5ff78dc707592bdb44
Yamikani Kalinde <yamikanikalinde@gmail.com>
2023-08-22 16:15:44 +0200

    add save user questions

--- src/HasSecurityQuestions.php
+++ src/HasSecurityQuestions.php
@@ -2,7 +2,30 @@
 
 namespace Bluecloud\SecurityQuestionHelpers;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait HasSecurityQuestions
 {
+    public function saveQuestions(array $questions)
+    {
+        /** @var Model $user */
+        $user = $this;
 
+        foreach ($questions as $question) {
+            $answer = hash_answer($question["answer"]);
+            $found = SecurityQuestionEntry::query()
+                ->where("user_id", $user->getKey())
+                ->where("security_question_id", $question["security_question_id"])
+                ->first();
+            if ($found) {
+                $found->update(["answer" => $answer]);
+            } else {
+                SecurityQuestionEntry::query()->create([
+                    "security_question_id" => $question["security_question_id"],
+                    "answer" => $answer,
+                    "user_id" => $user->getKey(),
+                ]);
+            }
+        }
+    }
 }
---
+++ src/Utilities/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!function_exists('hash_answer')) {
+    function hash_answer($answer): string
+    {
+        return $answer;
+    }
+}
